8df5dc1bdd9c82029766c9099920b518f85b5bf9
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2014-09-28 20:29:57 +0900

    delete App interface

--- ksatriya.go
+++ ksatriya.go
@@ -10,40 +10,22 @@ type Params struct {
 	httprouter.Params
 }
 
-type App interface {
-	Router() *httprouter.Router
-	View() ResultBuilder
-
-	Handle(method, path string, handler HandlerFunc, filters map[string]FilterFunc)
-	ServeHTTP(w http.ResponseWriter, req *http.Request)
-	Run(addr string)
-	RegisterController(d Dispacher)
-}
-
 type Ksatriya struct {
-	router *httprouter.Router
-	view   ResultBuilder
+	Router *httprouter.Router
+	View   ResultBuilder
 }
 
 func New() *Ksatriya {
 	k := &Ksatriya{
-		router: httprouter.New(),
-		view:   NewView(),
+		Router: httprouter.New(),
+		View:   NewView(),
 	}
 	return k
 }
 
-func (k *Ksatriya) Router() *httprouter.Router {
-	return k.router
-}
-
-func (k *Ksatriya) View() ResultBuilder {
-	return k.view
-}
-
 func (k *Ksatriya) Handle(method, path string, handler HandlerFunc, filters map[string]FilterFunc) {
-	k.Router().Handle(method, path, func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
-		ctx := NewContext(req, Params{params}, k.View())
+	k.Router.Handle(method, path, func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		ctx := NewContext(req, Params{params}, k.View)
 		if filter, ok := filters[BeforeFilterKey]; ok {
 			filter(ctx)
 		}
@@ -56,11 +38,11 @@ func (k *Ksatriya) Handle(method, path string, handler HandlerFunc, filters map[
 }
 
 func (k *Ksatriya) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	k.Router().ServeHTTP(w, req)
+	k.Router.ServeHTTP(w, req)
 }
 
 func (k *Ksatriya) Run(addr string) {
-	if err := http.ListenAndServe(addr, k.Router()); err != nil {
+	if err := http.ListenAndServe(addr, k.Router); err != nil {
 		panic(err)
 	}
 }
