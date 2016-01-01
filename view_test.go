425c2d38f250a7fe69c5e83ab96f6a99dc1b444f
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2016-01-01 13:53:54 +0900

    update test for ksatriya.View

--- view_test.go
+++ view_test.go
@@ -7,19 +7,26 @@ import (
 )
 
 func TestView(t *testing.T) {
+	// new view
 	v := NewView()
-
 	assert.Zero(t, v.Renderer())
 
+	// set text renderer
 	tr := NewTextRenderer("text")
 	v.SetRenderer(tr)
+	assert.Equal(t, tr, v.Renderer())
 
+	// render text
 	assert.Equal(t, tr.Render(), v.Render())
 
 	j := map[string]string{}
 	j["key"] = "value"
+
+	// set json renderer
 	jr := NewJSONRenderer(j)
 	v.SetRenderer(jr)
+	assert.Equal(t, jr, v.Renderer())
 
+	// render json
 	assert.Equal(t, jr.Render(), v.Render())
 }
