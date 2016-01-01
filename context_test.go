d832e018c76892957f919f9e785cb7553fcbccf6
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2016-01-02 01:51:41 +0900

    update test for ksatriya.Context

--- context_test.go
+++ context_test.go
@@ -10,10 +10,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func newTestContext() Ctx {
+	rec := httptest.NewRecorder()
+	req := &http.Request{}
+
+	return NewContext(rec, req, Args{})
+}
+
 func TestContext(t *testing.T) {
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/", nil)
-	assert.NoError(t, err)
+	req := &http.Request{}
 
 	arg := httprouter.Param{
 		Key:   "arg_key",
@@ -25,7 +31,6 @@ func TestContext(t *testing.T) {
 	v.Set("param_key", "param_value")
 	req.PostForm = v
 
-	// new context
 	ctx := NewContext(rec, req, Args{args})
 	assert.Equal(t, &Request{req}, ctx.Req())
 	assert.Equal(t, NewResponse(), ctx.Res())
@@ -34,4 +39,48 @@ func TestContext(t *testing.T) {
 	assert.Equal(t, "arg_value", ctx.Arg("arg_key"))
 	assert.Equal(t, Params{"param_key": []string{"param_value"}}, ctx.Params())
 	assert.Equal(t, "param_value", ctx.ParamSingle("param_key"))
+}
+
+func TestContext_RenderText(t *testing.T) {
+	ctx := newTestContext()
+
+	ctx.RenderText(http.StatusOK, "text")
+	assert.Equal(t, http.StatusOK, ctx.Res().StatusCode())
+	assert.Equal(t, "text/plain", ctx.Res().Header().Get("Content-Type"))
+	assert.Equal(t, NewTextRenderer("text"), ctx.View().Renderer())
+
+	rec := httptest.NewRecorder()
+
+	ctx.Write(rec)
+	assert.Equal(t, "text", rec.Body.String())
+}
+
+func TestContext_RenderJSON(t *testing.T) {
+	ctx := newTestContext()
+
+	j := map[string]string{"key": "value"}
+
+	ctx.RenderJSON(http.StatusOK, j)
+	assert.Equal(t, http.StatusOK, ctx.Res().StatusCode())
+	assert.Equal(t, "application/json", ctx.Res().Header().Get("Content-Type"))
+	assert.Equal(t, NewJSONRenderer(j), ctx.View().Renderer())
+
+	rec := httptest.NewRecorder()
+
+	ctx.Write(rec)
+	assert.Equal(t, "{\"key\":\"value\"}", rec.Body.String())
+}
+
+func TestContext_Redirect(t *testing.T) {
+	ctx := newTestContext()
+
+	ctx.Redirect("/redirect")
+	assert.Equal(t, http.StatusFound, ctx.Res().StatusCode())
+	assert.Equal(t, "/redirect", ctx.Res().Header().Get("Location"))
+	assert.Equal(t, NewTextRenderer(""), ctx.View().Renderer())
+
+	rec := httptest.NewRecorder()
+
+	ctx.Write(rec)
+	assert.Empty(t, rec.Body.String())
 }
