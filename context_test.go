398a697050c475997ed6d312b121de932aef3fe9
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2016-01-01 21:56:42 +0900

    update test for ksatriya.Context

--- context_test.go
+++ context_test.go
@@ -3,15 +3,35 @@ package ksatriya
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/stretchr/testify/assert"
 )
 
-func newTestContext(t *testing.T) Ctx {
+func TestContext(t *testing.T) {
 	rec := httptest.NewRecorder()
 	req, err := http.NewRequest("GET", "/", nil)
 	assert.NoError(t, err)
 
-	return NewContext(rec, req, Args{})
+	arg := httprouter.Param{
+		Key:   "arg_key",
+		Value: "arg_value",
+	}
+	args := httprouter.Params{arg}
+
+	v := url.Values{}
+	v.Set("param_key", "param_value")
+	req.PostForm = v
+
+	// new context
+	ctx := NewContext(rec, req, Args{args})
+	assert.Equal(t, &Request{req}, ctx.Req())
+	assert.Equal(t, NewResponse(), ctx.Res())
+	assert.Equal(t, NewView(), ctx.View())
+	assert.Equal(t, Args{args}, ctx.Args())
+	assert.Equal(t, "arg_value", ctx.Arg("arg_key"))
+	assert.Equal(t, Params{"param_key": []string{"param_value"}}, ctx.Params())
+	assert.Equal(t, "param_value", ctx.ParamSingle("param_key"))
 }
