41d48c82fb1da9dc2d70f67a8afc3b5034ff7bbd
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2016-01-01 13:55:08 +0900

    update test for ksatriya.Root

--- root_test.go
+++ root_test.go
@@ -8,14 +8,14 @@ import (
 )
 
 func TestRoot(t *testing.T) {
+	// new root
 	root := NewRoot()
-
 	assert.Equal(t, RootPathDefault+"/*filepath", root.Path())
 	assert.Equal(t, http.Dir(RootDirDefault), root.Dir())
 
+	// set path & dir
 	root.SetPath("/public")
 	root.SetDir("public")
-
 	assert.Equal(t, "/public/*filepath", root.Path())
 	assert.Equal(t, http.Dir("public"), root.Dir())
 }
