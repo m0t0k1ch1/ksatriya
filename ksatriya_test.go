13e1f1ac6e2df19d12124e58093dc18a6abc3b7e
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2016-01-02 11:09:54 +0900

    update test for ksatriya.Ksatriya

--- ksatriya_test.go
+++ ksatriya_test.go
@@ -3,26 +3,63 @@ package ksatriya
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestKsatriya_ServeHTTP(t *testing.T) {
+func TestKsatriya(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req := &http.Request{}
+
+	// new ksatriya
 	k := New()
-	k.GET("/user/:name", func(ctx Ctx) {
-		name := ctx.Arg("name")
-		res := fmt.Sprintf("your name is %s", name)
-		ctx.RenderText(http.StatusOK, res)
+	assert.Equal(t, NewRouter(), k.Router())
+	assert.Equal(t, NewRoot(), k.Root())
+	assert.Equal(t, NewContext(rec, req, Args{}), k.ctxBuilder(rec, req, Args{}))
+
+	// add routes
+	k.GET("/get", func(ctx Ctx) {
+		ctx.RenderText(http.StatusOK, "get")
+	})
+	k.POST("/post", func(ctx Ctx) {
+		ctx.RenderText(http.StatusOK, "post")
+	})
+	k.PUT("/put", func(ctx Ctx) {
+		ctx.RenderText(http.StatusOK, "put")
+	})
+	k.PATCH("/patch", func(ctx Ctx) {
+		ctx.RenderText(http.StatusOK, "patch")
+	})
+	k.DELETE("/delete", func(ctx Ctx) {
+		ctx.RenderText(http.StatusOK, "delete")
 	})
 
 	h := k.ServeHTTP
 
-	assert.HTTPSuccess(t, h, "GET", "/user/m0t0k1ch1", nil)
-	assert.HTTPBodyContains(t, h, "GET", "/user/m0t0k1ch1", nil, "your name is m0t0k1ch1")
+	// GET /get
+	assert.HTTPSuccess(t, h, "GET", "/get", nil)
+	assert.HTTPBodyContains(t, h, "GET", "/get", nil, "get")
+
+	// POST /post
+	assert.HTTPSuccess(t, h, "POST", "/post", nil)
+	assert.HTTPBodyContains(t, h, "POST", "/post", nil, "post")
+
+	// PUT /put
+	assert.HTTPSuccess(t, h, "PUT", "/put", nil)
+	assert.HTTPBodyContains(t, h, "PUT", "/put", nil, "put")
+
+	// PATCH /patch
+	assert.HTTPSuccess(t, h, "PATCH", "/patch", nil)
+	assert.HTTPBodyContains(t, h, "PATCH", "/patch", nil, "patch")
+
+	// DELETE /delete
+	assert.HTTPSuccess(t, h, "DELETE", "/delete", nil)
+	assert.HTTPBodyContains(t, h, "DELETE", "/delete", nil, "delete")
 }
 
-func TestKsatriya_ServeHTTP_redirection(t *testing.T) {
+func TestKsatriya_redirection(t *testing.T) {
 	k := New()
 	k.GET("/", func(ctx Ctx) {
 		ctx.RenderText(http.StatusOK, "")
@@ -36,7 +73,21 @@ func TestKsatriya_ServeHTTP_redirection(t *testing.T) {
 	assert.HTTPRedirect(t, h, "GET", "/redirect", nil)
 }
 
-func TestKsatriya_ServeHTTP_withController(t *testing.T) {
+func TestKsatriya_withArgs(t *testing.T) {
+	k := New()
+	k.GET("/user/:name", func(ctx Ctx) {
+		name := ctx.Arg("name")
+		res := fmt.Sprintf("your name is %s", name)
+		ctx.RenderText(http.StatusOK, res)
+	})
+
+	h := k.ServeHTTP
+
+	assert.HTTPSuccess(t, h, "GET", "/user/m0t0k1ch1", nil)
+	assert.HTTPBodyContains(t, h, "GET", "/user/m0t0k1ch1", nil, "your name is m0t0k1ch1")
+}
+
+func TestKsatriya_withController(t *testing.T) {
 	c := NewController()
 	c.GET("/ping", func(ctx Ctx) {
 		ctx.RenderText(http.StatusOK, "pong")
